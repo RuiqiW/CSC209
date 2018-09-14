b7c87620fa0464fd0181697d27fc3bccacabb096
Ruiqi Wang <ruiqi.wang@mail.utoronto.ca>
2018-09-14 10:32:09 -0400

    sh files

--- Lab1/my_commands.sh
+++ Lab1/my_commands.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-gcc -Wall -std=gnu99 -g -o echo_out.txt echo_arg.c
-echo_out.txt csc209
-gcc -Wall -std=gnu99 -g -o echo_stdin echo_stdin.c
+echo_arg csc209 > echo_out.txt
 echo_stdin < echo_stdin.c
-gcc -Wall -std=gnu99 -g -o count count.c
 count 210 | wc -c
-gcc -Wall -std=gnu99 -g -o echo_stdin echo_stdin.c
 ls -S | echo_stdin
