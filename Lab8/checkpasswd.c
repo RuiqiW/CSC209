16a38fb989f7cb1021b92bae551e6ef670c34fc1
Ruiqi Wang <ruiqi.wang@mail.utoronto.ca>
2018-11-01 20:16:55 -0400

    Lab8

--- Lab8/checkpasswd.c
+++ Lab8/checkpasswd.c
@@ -24,8 +24,69 @@ int main(void) {
       perror("fgets");
       exit(1);
   }
-  
 
+  // pipe
+  int fd[2];
+  if(pipe(fd) == -1){
+        perror("pipe");
+        exit(1);
+    }
+ 
+  // fork
+  int result = fork();
+
+  // child process
+  if(result == 0){
+
+      close(fd[1]);
+
+      if(dup2(fd[0], fileno(stdin)) == -1){
+          perror("dup2");
+          exit(1);
+      }
+      execl("./validate", "validate", NULL);
+      perror("exec");
+      exit(1);
+  
+  }else if(result > 0){ // parent process
+      
+      close(fd[0]);
+
+      if(write(fd[1], user_id, MAXLINE) == -1){
+          perror("write to pipe");
+          exit(1);
+      }
+      if(write(fd[1], password, MAXLINE) == -1){
+          perror("write to pipe");
+          exit(1);
+      }
+      
+      close(fd[1]);
+      
+      
+  }else{ // fork failed
+      perror("fork");
+      exit(1);
+  }
+
+  pid_t pid;
+  int status;
+  
+  if((pid = wait(&status)) == -1){
+      perror("wait");
+    }else{
+        if(WIFEXITED(status)){
+            if(WEXITSTATUS(status) == 1){
+                exit(1);
+            }else if(WEXITSTATUS(status) == 2){
+                printf(INVALID);
+            }else if(WEXITSTATUS(status) == 3){
+                printf(NO_USER);
+            }
+        }else{
+            printf(SUCCESS);
+        }
+    }
 
   return 0;
 }
