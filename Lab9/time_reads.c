300e476c70b24d75d0918c88bd1f5434a4d0e455
Ruiqi Wang <ruiqi.wang@mail.utoronto.ca>
2018-11-15 19:53:12 -0500

    Lab9

--- Lab9/time_reads.c
+++ Lab9/time_reads.c
@@ -15,6 +15,13 @@
  */
 long num_reads, seconds;
 
+/* A new handler to handle SIGPROF signal.
+*/
+void handler(int code){
+    fprintf(stderr, MESSAGE, num_reads, seconds);
+    exit(0);
+}
+
 
 /* The first command-line argument is the number of seconds to set a timer to run.
  * The second argument is the name of a binary file containing 100 ints.
@@ -34,14 +41,31 @@ int main(int argc, char **argv) {
       exit(1);
     }
 
+    // set sigaction
+    struct sigaction newact;
+    newact.sa_handler = handler;
+    newact.sa_flags = 0;
+    sigemptyset(&newact.sa_mask);
+    sigaction(SIGPROF, &newact, NULL);
+
+    // set timer
+    struct itimerval newtimer;
+    newtimer.it_interval.tv_sec = 0;
+    newtimer.it_interval.tv_usec = 0;
+    newtimer.it_value.tv_sec = seconds;
+    newtimer.it_value.tv_usec = 0;
+    setitimer(ITIMER_PROF, &newtimer, NULL);
+
     /* In an infinite loop, read an int from a random location in the file,
      * and print it to stderr.
      */
     for (;;) {
-
-
-
-
+      int r = random() % 100;
+      int num;
+      fseek(fp, r * sizeof(int), SEEK_SET);
+      fread(&num, sizeof(int), 1, fp);
+      fprintf(stderr, "%d ", num);
+      num_reads ++;
     }
     return 1; // something is wrong if we ever get here!
 }
--- Lab9/write_test_file.c
+++ Lab9/write_test_file.c
@@ -19,10 +19,10 @@ int main(int argc, char **argv) {
     }
 
     // TODO: complete this program according its description above.
-
-
-
-
+    for(int i = 0; i < 100; i++){
+        int r = random() % 100;
+        fwrite(&r, sizeof(int), 1, fp);
+    }
 
     fclose(fp);
     return 0;
